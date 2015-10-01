6ed53ee928d86f2de129546499940abd9a805f08
sandile <msentri@gmail.com>
2015-10-01 16:16:00 +0200

    Adding map and cleaning the flow and add code for action and map restructure online database add more field on registration using trusted okhttp class restructure profile screen

--- app/src/main/java/com/example/sandilemazibuko/grooveapp_beta_01/Login.java
+++ app/src/main/java/com/example/sandilemazibuko/grooveapp_beta_01/Login.java
@@ -3,86 +3,93 @@ package com.example.sandilemazibuko.grooveapp_beta_01;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
+
 public class Login extends AppCompatActivity {
 
-    EditText txtuser,txtpass;
-    Button btnLog;
-    JSONObject saObj;
+
+    EditText txtEmail,txtPassword;
+    String email = null,password = null;
 
     // Progress Dialog
     private ProgressDialog pDialog;
 
     LocalStorage userDatabase;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
-
         userDatabase = new LocalStorage(this);
 
-        btnLog = (Button) findViewById(R.id.btnlog);
+         txtEmail = (EditText) findViewById(R.id.txtemail);
+         txtPassword = (EditText) findViewById(R.id.txtpass);
 
-        txtuser = (EditText) findViewById(R.id.txtuser);
-        txtpass = (EditText) findViewById(R.id.txtpass);
+         email = txtEmail.getText().toString();
+         password = txtPassword.getText().toString();
 
+        Button btnLogin = (Button) findViewById(R.id.btnlog);
 
-
-        btnLog.setOnClickListener(new View.OnClickListener() {
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!txtpass.getText().toString().equals("")) {
-                    if (!txtuser.getText().toString().equals("") ) {
-                        if (isOnline()) {
-                            requestData("http://groovapp.msentri.co.za/get_user.php",txtuser.getText().toString(),txtpass.getText().toString() );
-                        } else {
-                            Toast.makeText(Login.this, "Network isn't available", Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        //txtpass.setError("Please Enter Password");
-                        Toast.makeText(Login.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
-                    }
-                }else{
-                    //txtuser.setError("Please Enter Username");
-                    Toast.makeText(Login.this,"Please Enter Username",Toast.LENGTH_SHORT).show();;
+            public void onClick(View v) {
 
+                txtEmail = (EditText) findViewById(R.id.txtemail);
+                txtPassword = (EditText) findViewById(R.id.txtpass);
+
+                email = txtEmail.getText().toString();
+                password = txtPassword.getText().toString();
+
+
+
+                if(!email.equals("")){
+                    if(!password.equals("")){
+                        if(isOnline()){
+                            String[] myTaskParams = {email,password};
+
+                            new LoginTask().execute(myTaskParams);
+
+                        }else{
+                            Toast.makeText(Login.this,"Your Device is not Connected",Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(Login.this,"Please Enter Password",Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(Login.this,"Please Enter Email",Toast.LENGTH_SHORT).show();
                 }
+
+
             }
         });
-    }
-
-    private void requestData(String uri, String username,String password) {
-
-        RequestPackage p = new RequestPackage();
-        p.setMethod("POST");
-        p.setUri(uri);
-        p.setParam("username", username);
-        p.setParam("password", password);
 
 
-        MyTask task = new MyTask();
-        task.execute(p);
+
     }
 
     protected boolean isOnline() {
@@ -95,11 +102,7 @@ public class Login extends AppCompatActivity {
         }
     }
 
-    /**
-     * THESE PRIVATE CLASS PROCESS THE CONNECTION FROM ONLINE WEB SERVICE SERVER TO ACCESS
-     * ONLINE DATABASE DOING IT IN THE BACKGROUND THREAD
-     * */
-    private class MyTask extends AsyncTask<RequestPackage, String, JSONObject> {
+    private class LoginTask extends AsyncTask<String, String, JSONObject> {
 
         /**
          * THESE FUNCTION PROCESS INFORMATION FROM FIRST TIME BEFORE RUNNING THE DO BACKGROUND TASK
@@ -114,21 +117,38 @@ public class Login extends AppCompatActivity {
             pDialog.show();
         }
 
-        /**
-         * THESE FUNCTION PROCESS DOES THE WHOLE FUNCTIONALITY ON ACCESSING THE ONLINE INFORMATION
-         * THAT IS BEING GENERATED AS JSON FORMAT AND PRESENT IT TO NODE FORMAT
-         * */
         @Override
-        protected JSONObject doInBackground(RequestPackage... params) {
-            String content = HttpManager.getData(params[0]);
-            try {
-                 saObj = new JSONObject(content);
-                //Log.v("******************************************* -  ", saObj.toString());
+        protected JSONObject doInBackground(String... params) {
+            OkHttpClient client = new OkHttpClient();
 
+            String url = "http://groovapp.msentri.co.za/get_user.php";
+
+
+            RequestBody formBody = new FormEncodingBuilder()
+                    .add("email", params[0])
+                    .add("password", params[1])
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(formBody)
+                    .build();
+
+            Response response = null;
+            JSONObject Jobject = null;
+            try {
+                response = client.newCall(request).execute();
+                String StringRespons = response.body().string();
+                Jobject = new JSONObject(StringRespons);
+
+            } catch (IOException e) {
+                e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return saObj;
+
+
+            return Jobject;
         }
 
         @Override
@@ -136,82 +156,46 @@ public class Login extends AppCompatActivity {
             // dismiss the dialog after getting all products
             pDialog.dismiss();
 
+                JSONArray Jarray = null;
             try {
-                String SERVER_RESPOND = result.getString("success");
-                if(!SERVER_RESPOND.equals("0")){
+                Jarray = result.getJSONArray("user");
+                JSONObject object = Jarray.getJSONObject(0);
 
-                    JSONArray jsonArray = result.getJSONArray("user");
-                    JSONObject finalJSONOBArray = jsonArray.getJSONObject(0);
 
-                    String user_id = finalJSONOBArray.getString("user_id");
-                    String user_name = finalJSONOBArray.getString("name");
-                    String user_surname = finalJSONOBArray.getString("surname");
-                    String user_id_number = finalJSONOBArray.getString("id_number");
-                    String user_email = finalJSONOBArray.getString("email");
-                    String user_cellphone = finalJSONOBArray.getString("cellphone");
-                    String user_username = finalJSONOBArray.getString("username");
-                    String password = finalJSONOBArray.getString("password");
 
-                    User user = new User(user_id,user_name,user_surname,user_id_number,user_email,
-                            user_cellphone,user_username,password);
-                    userDatabase.storeUserDetailsOnPreference(user);
 
-                    //Toast.makeText(Login.this,"User Logged in",Toast.LENGTH_LONG).show();
+
+
+                if(!object.getString("name").equals("")) {
+                    Toast.makeText(Login.this, object.getString("name"), Toast.LENGTH_SHORT).show();
+
+                    //setContentView(R.layout.activity_profile);
+//                    String user_id = object.getString("user_id");
+//                    String user_name = object.getString("name");
+//                    String user_surname = object.getString("surname");
+//                    String user_picture = object.getString("user_picture");
+//                    String user_password = object.getString("user_password");
+//                    String user_email = object.getString("user_email");
+//                    String user_dob = object.getString("user_dob");
+//                    String user_membership_type = object.getString("user_membership_type");
+//
+//                    User user = new User(user_id,user_name,user_surname,user_picture,user_password,
+//                            user_email,user_dob,user_membership_type);
+//                    userDatabase.storeUserDetailsOnPreference(user);
+
 
                     Intent intent = new Intent(Login.this, Profile.class);
                     startActivity(intent);
-
-                    //Log.v("--------------------------------------- ", user_cellphone);
-                    //Toast.makeText(Login.this,user_email,Toast.LENGTH_LONG).show();
-
-
-                }else{
-                    Toast.makeText(Login.this,"User Not Found",Toast.LENGTH_LONG).show();
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
         }
-
     }
 
-    /**
-     * -------------------------------------------------------------------------------------------------------------
-     * MENU NAVIGATION
-     * -------------------------------------------------------------------------------------------------------------
-     * */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        userDatabase = new LocalStorage(this);
 
 
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(Login.this, "Settings", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.action_about_this_app){
-            Toast.makeText(Login.this, "About This App", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.action_instructions){
-            Toast.makeText(Login.this, "App Instructions", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.action_faqs){
-            Toast.makeText(Login.this, "FAQ's", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.action_logout){
-            userDatabase.clearUserData();
-            Intent intent = new Intent(Login.this, MainActivity.class);
-            startActivity(intent);
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
